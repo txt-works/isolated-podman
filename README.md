1c25f1f278a64acd3ecfaed1a110d363671c463d
Test Account <139959309+copy-paste-repeat@users.noreply.github.com>
2025-01-26 19:31:16 +0000

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# isolated-podman
+A Nix Flake setup that ensures configuration is isolated and does not inherit system-wide settings
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Configurable isolated Podman setup";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mkPodmanWrapper = pkgs.writeShellScriptBin "podman" ''
+          current_dir="$(pwd)"
+          export HOME="$current_dir/.podman"
+          mkdir -p "$HOME/.config/containers"
+          cp "$current_dir/.config/containers/policy.json" "$HOME/.config/containers/"
+          cp "$current_dir/.config/containers/registries.conf" "$HOME/.config/containers/"
+          exec ${pkgs.podman}/bin/podman "$@"
+        '';
+      in {
+        packages.default = mkPodmanWrapper;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            mkPodmanWrapper
+            pkgs.runc
+            pkgs.conmon
+            pkgs.slirp4netns
+            pkgs.fuse-overlayfs
+          ];
+        };
+      });
+}
