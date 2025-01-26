9acfa0151f491c10b9ff71de96868d7eb58abba9
Test Account <139959309+copy-paste-repeat@users.noreply.github.com>
2025-01-26 21:13:28 +0000

    fix resolving config

--- flake.nix
+++ flake.nix
@@ -9,12 +9,18 @@
       let
         pkgs = import nixpkgs { inherit system; };
         mkPodmanWrapper = pkgs.writeShellScriptBin "podman" ''
+
+          # TODO: need optimisation: move out and built / install stage only (trigger each call of podman, not efficient approach)
           current_dir="$(pwd)"
           export HOME="$current_dir/.state/podman/"
           mkdir -p "$HOME/.config/podman"
-          # TODO: need optimisation and move out and built / install stage only
-          cp "$current_dir/.config/podman/containers.conf" "$HOME/.config/podman/containers.conf"
+          mkdir -p "$HOME/.config/containers"
+
+          export CONTAINERS_REGISTRIES_CONF="$HOME/.config/podman/registries.conf"
+          env
+
           cp "$current_dir/.config/podman/registries.conf" "$HOME/.config/podman/registries.conf"
+          cp "$current_dir/.config/containers/containers.conf" "$HOME/.config/containers/containers.conf"
           cp "$current_dir/.config/containers/policy.json" "$HOME/.config/containers/policy.json"
           exec ${pkgs.podman}/bin/podman "$@"
         '';
