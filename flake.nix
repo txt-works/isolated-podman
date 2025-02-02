1db1f50e0bcea9cf540c32269ae60abad9d9884d
Test Account <139959309+copy-paste-repeat@users.noreply.github.com>
2025-02-02 16:15:55 +0000

    add setcap wrapper

--- flake.nix
+++ flake.nix
@@ -1,48 +1,63 @@
 {
- description = "Configurable isolated Podman setup";
- inputs = {
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-   flake-utils.url = "github:numtide/flake-utils";
- };
- outputs = { self, nixpkgs, flake-utils }: 
-   flake-utils.lib.eachDefaultSystem (system:
-     let
-       pkgs = import nixpkgs { inherit system; };
-       configFiles = pkgs.stdenv.mkDerivation {
-         name = "podman-config";
-         src = ./config;
-         
-         installPhase = ''
-           mkdir -p $out/config
-           cp -r * $out/config/
-         '';
-       };
-       mkPodmanWrapper = pkgs.writeShellScriptBin "podman" ''
-         current_dir="$(pwd)" 
-         export HOME="$current_dir/.state/podman"
-         mkdir -p "$HOME/.config/podman"
-         mkdir -p "$HOME/.config/containers"
+  description = "Configurable isolated Podman setup with rootless port binding";
 
-         if [ ! -f "$HOME/.config/podman/registries.conf" ]; then
-           cp ${configFiles}/config/podman/registries.conf "$HOME/.config/podman/"
-           cp ${configFiles}/config/containers/containers.conf "$HOME/.config/containers/"
-           cp ${configFiles}/config/containers/policy.json "$HOME/.config/containers/"
-         fi
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-         export CONTAINERS_REGISTRIES_CONF="$HOME/.config/podman/registries.conf"
-         exec ${pkgs.podman}/bin/podman "$@"
-       '';
-     in {
-       packages.default = mkPodmanWrapper;
-       packages.configFiles = configFiles;
-       devShell = pkgs.mkShell {
-         buildInputs = [
-           mkPodmanWrapper
-           pkgs.runc
-           pkgs.conmon
-           pkgs.slirp4netns
-           pkgs.fuse-overlayfs
-         ];
-       };
-     });
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        configFiles = pkgs.stdenv.mkDerivation {
+          name = "podman-config";
+          src = ./config;
+          
+          installPhase = ''
+            mkdir -p $out/config
+            cp -r * $out/config/
+          '';
+        };
+
+        podmanWithCaps = pkgs.runCommand "podman-with-caps" {
+          nativeBuildInputs = [ pkgs.libcap ];
+        } ''
+          cp -r ${pkgs.podman} $out
+          setcap cap_net_bind_service=+ep $out/bin/podman
+        '';
+
+        mkPodmanWrapper = pkgs.writeShellScriptBin "podman" ''
+          current_dir="$(pwd)" 
+          export HOME="$current_dir/.state/podman"
+          mkdir -p "$HOME/.config/podman"
+          mkdir -p "$HOME/.config/containers"
+
+          if [ ! -f "$HOME/.config/podman/registries.conf" ]; then
+            cp ${configFiles}/config/podman/registries.conf "$HOME/.config/podman/"
+            cp ${configFiles}/config/containers/containers.conf "$HOME/.config/containers/"
+            cp ${configFiles}/config/containers/policy.json "$HOME/.config/containers/"
+          fi
+
+          export CONTAINERS_REGISTRIES_CONF="$HOME/.config/podman/registries.conf"
+
+          exec ${podmanWithCaps}/bin/podman "$@"
+        '';
+      in {
+        packages.default = mkPodmanWrapper;
+        packages.configFiles = configFiles;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            mkPodmanWrapper
+            podmanWithCaps
+            pkgs.runc
+            pkgs.conmon
+            pkgs.slirp4netns
+            pkgs.fuse-overlayfs
+            pkgs.libcap # Needed for setcap
+          ];
+        };
+      });
 }
