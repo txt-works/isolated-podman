62aaa644c76c9ee8c30f4abb32b8782c12ffdcfa
Test Account <139959309+copy-paste-repeat@users.noreply.github.com>
2025-02-02 16:38:27 +0000

    test podman root mode

--- flake.nix
+++ flake.nix
@@ -1,16 +1,13 @@
 {
-  description = "Configurable isolated Podman setup with rootless port binding";
-
+  description = "Configurable isolated Podman setup";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
         configFiles = pkgs.stdenv.mkDerivation {
           name = "podman-config";
           src = ./config;
@@ -20,15 +17,13 @@
             cp -r * $out/config/
           '';
         };
-
-        podmanWithCaps = pkgs.runCommand "podman-with-caps" {
-          nativeBuildInputs = [ pkgs.libcap ];
-        } ''
-          cp -r ${pkgs.podman} $out
-          setcap cap_net_bind_service=+ep $out/bin/podman
-        '';
-
         mkPodmanWrapper = pkgs.writeShellScriptBin "podman" ''
+          # Check if running as root
+          if [ "$(id -u)" != "0" ]; then
+            echo "This script must be run as root"
+            exit 1
+          fi
+          
           current_dir="$(pwd)" 
           export HOME="$current_dir/.state/podman"
           mkdir -p "$HOME/.config/podman"
@@ -41,22 +36,23 @@
           fi
 
           export CONTAINERS_REGISTRIES_CONF="$HOME/.config/podman/registries.conf"
-
-          exec ${podmanWithCaps}/bin/podman "$@"
+          
+          # Set required capabilities
+          ${pkgs.libcap}/bin/setcap cap_sys_admin,cap_net_admin,cap_sys_chroot+ep ${pkgs.podman}/bin/podman
+          
+          exec ${pkgs.podman}/bin/podman "$@"
         '';
       in {
         packages.default = mkPodmanWrapper;
         packages.configFiles = configFiles;
-
         devShell = pkgs.mkShell {
           buildInputs = [
             mkPodmanWrapper
-            podmanWithCaps
             pkgs.runc
             pkgs.conmon
             pkgs.slirp4netns
             pkgs.fuse-overlayfs
-            pkgs.libcap # Needed for setcap
+            pkgs.libcap  # Added for setcap
           ];
         };
       });
